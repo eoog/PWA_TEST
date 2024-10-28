18b109e2222e9232ff47d46cec48cef51ef71f97
sookyeong <sookyeong0505@gmail.com>
2024-10-28 12:39:11 +0900

    스피너 가운데로 이동

--- src/components/TextViewWrapper.js
+++ src/components/TextViewWrapper.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
-import {checkExtensionInstalled} from "./extensionCheck";
+import { checkExtensionInstalled } from "./extensionCheck";
 import TextView from "../view/TextView";
 
 const TextViewWrapper = () => {
@@ -17,7 +17,7 @@ const TextViewWrapper = () => {
 
     if (isExtensionInstalled === null) {
         return (
-            <div className="min-h-screen flex items-center justify-center">
+            <div className="flex-1 h-full w-full flex items-center justify-center">
                 <div className="animate-spin h-8 w-8 border-4 border-blue-500 rounded-full border-t-transparent"></div>
             </div>
         );
