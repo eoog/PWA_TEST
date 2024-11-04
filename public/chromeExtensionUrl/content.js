32f0f100c753aeecdc9af48e07de6e0ee45c2d39
en <qkqhemforhs1@gmail.com>
2024-11-04 11:47:25 +0900

    1. 옵저버 변경 및 확장프로그램 알집 수정

--- public/chromeExtensionUrl/content.js
+++ public/chromeExtensionUrl/content.js
@@ -33,12 +33,31 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
+// // DOM 변화 감지
+// const observer = new MutationObserver(() => {
+//   // 탭의 변화된 내용을 감지하고 background script에 요청
+//   console.log("변화 감지!");
+//   chrome.runtime.sendMessage({ type: "REQUEST_TABS_DATA" });
+// });
+//
+// // body 태그의 변화를 감지
+// observer.observe(document.body, { childList: true, subtree: true, characterData: true });
+
 // DOM 변화 감지
-const observer = new MutationObserver(() => {
-  // 탭의 변화된 내용을 감지하고 background script에 요청
-  console.log("변화 감지!");
-  chrome.runtime.sendMessage({ type: "REQUEST_TABS_DATA" });
+const observer = new MutationObserver((mutationsList) => {
+  // 변화가 감지되었을 때 처리
+  mutationsList.forEach((mutation) => {
+    if (mutation.type === 'childList' || mutation.type === 'characterData') {
+
+      // 백그라운드 스크립트에 데이터 요청
+      chrome.runtime.sendMessage({ type: "REQUEST_TABS_DATA" });
+    }
+  });
 });
 
 // body 태그의 변화를 감지
-observer.observe(document.body, { childList: true, subtree: true, characterData: true });
+observer.observe(document.body, {
+  childList: true,  // 자식 요소의 추가/제거 감지
+  subtree: true,    // 하위 요소도 감지
+  characterData: true // 텍스트 노드의 변화 감지
+});
